ac53117af1741ac301cc8c2d4b1fb60499e1aba5
Shadman <shadman@pdx.edu>
2017-04-05 17:33:14 -0700

    Homework 1 - 1bit adder

---
+++ homework1/onebitAdder.sv
@@ -0,0 +1,17 @@
+module onebitAdder ( input A,
+	   	     input B,
+		     input Cin,
+		     output S,
+		     output Cout);
+wire N1,N2,N3; 
+
+	     
+XNOR sum1(N1,A,B); 
+XNOR sumout(S,N1,Cin); 
+
+AND carry1(N2, A, Cin);
+AND carry2(N3, B, Cin); 
+
+OR  carryout(Cout, N2, N3);
+
+endmodule 
