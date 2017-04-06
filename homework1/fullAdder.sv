5f102028fb4c48b43b5f3239db3e1e8535c9d623
Shadman <shadman@pdx.edu>
2017-04-05 19:36:43 -0700

    One bit adder and full bit adder compiled error free

--- homework1/fullAdder.sv
+++ homework1/fullAdder.sv
@@ -1,13 +1,19 @@
-module fullAdder #(  parameter Width = 64);
-		   ( input [width-1:0] A,
-	   	     input [width-1:0] B,
-		     input Cin,
-		     output [width-1:0] S,
-		     output Cout);
+module fullAdder(A,B,Cin,S,Cout); 
 
-logic [Width-1:0] Nwires; 
+parameter width = 64; 
 
-for(int i = 0; i < width; i++); 
+	
+input [width-1:0] A;
+input [width-1:0] B;
+input Cin;
+output [width-1:0] S;
+output Cout;
+
+logic [width-1:0] Nwires; 
+genvar i;
+
+generate
+for(i = 0; i < width; i++)
 begin: onebitAdder_initilization 
 	onebitAdder(
 		.A(A[i]),
@@ -16,8 +22,7 @@ begin: onebitAdder_initilization
 		.S(S[i]),
 		.Cout(Cout)
 	);
-
-
 end
+endgenerate 
 
 endmodule 
